1e5f3c98053f49a4f92c6374bb553ce08dd7f2e1
Domica <Domica@users.noreply.github.com>
2026-01-23 15:32:59 +0100

    Update DesignData.cs
    Corection of service registration

--- StabilityMatrix.Avalonia/DesignData/DesignData.cs
+++ StabilityMatrix.Avalonia/DesignData/DesignData.cs
@@ -148,9 +148,6 @@ public static class DesignData
             .AddSingleton<IImageIndexService, MockImageIndexService>()
             .AddSingleton<IMetadataImportService, MetadataImportService>();
 
-            services.AddSingleton<TiledVAECardViewModel>();
-
-
         // Placeholder services that nobody should need during design time
         services
             .AddSingleton<IPyRunner>(_ => null!)
