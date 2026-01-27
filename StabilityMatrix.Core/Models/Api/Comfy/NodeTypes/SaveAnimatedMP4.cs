9c6d347960ff9499c7fd7dac883c30093751afe7
Domica <Domica@users.noreply.github.com>
2026-01-27 14:12:33 +0100

    Create SaveAnimatedMP4.cs

---
+++ StabilityMatrix.Core/Models/Api/Comfy/NodeTypes/SaveAnimatedMP4.cs
@@ -0,0 +1,8 @@
+using StabilityMatrix.Core.Attributes;
+using StabilityMatrix.Core.Models.Api.Comfy.Nodes;
+
+namespace ComfyNodeBuilder;
+
+[ComfyNode("SaveAnimatedMP4")]
+public record SaveAnimatedMP4 
+    : global::StabilityMatrix.Core.Models.Api.Comfy.Nodes.SaveAnimatedMP4;
