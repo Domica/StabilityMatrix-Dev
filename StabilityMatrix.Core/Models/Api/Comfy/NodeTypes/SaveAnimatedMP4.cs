5e94398451129a037417c70fadae5105db5a5be2
Domica <Domica@users.noreply.github.com>
2026-01-27 15:29:22 +0100

    Update SaveAnimatedMP4.cs

--- StabilityMatrix.Core/Models/Api/Comfy/NodeTypes/SaveAnimatedMP4.cs
+++ StabilityMatrix.Core/Models/Api/Comfy/NodeTypes/SaveAnimatedMP4.cs
@@ -4,5 +4,5 @@ using StabilityMatrix.Core.Models.Api.Comfy.Nodes;
 namespace StabilityMatrix.Core.Models.Api.Comfy.NodeTypes;
 
 [ComfyNode("SaveAnimatedMP4")]
-public record SaveAnimatedMP4 
+public record SaveAnimatedMP4
     : global::StabilityMatrix.Core.Models.Api.Comfy.Nodes.SaveAnimatedMP4;
