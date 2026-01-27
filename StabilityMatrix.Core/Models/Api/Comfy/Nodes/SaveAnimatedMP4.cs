f9350c918a2a1c01a7b042a67740f2aba90dd43e
Domica <Domica@users.noreply.github.com>
2026-01-27 18:08:13 +0100

    Update SaveAnimatedMP4.cs

--- StabilityMatrix.Core/Models/Api/Comfy/Nodes/SaveAnimatedMP4.cs
+++ StabilityMatrix.Core/Models/Api/Comfy/Nodes/SaveAnimatedMP4.cs
@@ -1,7 +1,9 @@
-namespace StabilityMatrix.Core.Models.Api.Comfy.Nodes;
-
-public record SaveAnimatedMP4
+public record SaveAnimatedMP4 : NamedComfyNode
 {
+    public new string Name { get; set; } = "SaveAnimatedMP4";
+
+    public SaveAnimatedMP4() : base("SaveAnimatedMP4") { }
+
     public required object Images { get; init; }
     public required string FilenamePrefix { get; init; }
     public required double Fps { get; init; }
