33044dc46298afb567079ad0989df8815e600b0b
Domica <Domica@users.noreply.github.com>
2026-01-29 18:17:42 +0100

    Update WanMemoryCleanupModule.cs

--- StabilityMatrix.Avalonia/ViewModels/Inference/Modules/WanMemoryCleanupModule.cs
+++ StabilityMatrix.Avalonia/ViewModels/Inference/Modules/WanMemoryCleanupModule.cs
@@ -1,52 +1,39 @@
-using StabilityMatrix.Avalonia.Models.Inference;
-using StabilityMatrix.Avalonia.Services;
 using StabilityMatrix.Avalonia.ViewModels.Base;
+using StabilityMatrix.Core.Models.Api.Comfy;
+using StabilityMatrix.Core.Models.Api.Comfy.Nodes;
 
 namespace StabilityMatrix.Avalonia.ViewModels.Inference.Modules;
 
 public class WanMemoryCleanupModule : ModuleBase
 {
-    private bool _enabled;
-
     public WanMemoryCleanupModule(IServiceManager<ViewModelBase> vmFactory)
         : base(vmFactory)
     {
         Title = "WAN Memory Cleanup";
-        IsEnabled = true;
-    }
-
-    public bool Enabled
-    {
-        get => _enabled;
-        set => SetProperty(ref _enabled, value);
+        IsEnabled = false;
     }
 
     protected override void OnApplyStep(ModuleApplyStepEventArgs e)
     {
-        if (!Enabled)
+        if (!IsEnabled)
             return;
 
         var builder = e.Builder;
 
-        var cleanupNode = new ComfyNode
+        // Generate unique node name
+        var name = builder.GetUniqueName("WANMemoryCleanup");
+
+        var cleanupNode = new NamedComfyNode(name)
         {
-            Type = "WANMemoryCleanupNode",
-            Id = builder.GenerateNodeId(),
-            Inputs = new Dictionary<string, object>
+            ClassType = "WANMemoryCleanupNode",
+            Inputs = new Dictionary<string, object?>
             {
-                { "anything", null },
-                { "offload_wan_models", true },
-                { "offload_cache", true }
+                ["anything"] = null,
+                ["offload_wan_models"] = true,
+                ["offload_cache"] = true
             }
         };
 
-        if (builder.Nodes.ContainsKey("WANInferenceNode"))
-        {
-            builder.InsertNodeAfter("WANInferenceNode", cleanupNode);
-        }
-        else
-        {
-            builder.Nodes.Add(cleanupNode.Id, cleanupNode);
-        }
+        builder.Nodes.Add(name, cleanupNode);
     }
 }
