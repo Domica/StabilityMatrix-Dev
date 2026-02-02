f094f2df162f3aebab0e5a9fcce8bc79d7ab62d1
Domica <Domica@users.noreply.github.com>
2026-02-02 09:37:08 +0100

    Update OutpaintCard.axaml.cs

--- StabilityMatrix.Avalonia/Controls/Inference/OutpaintCard.axaml.cs
+++ StabilityMatrix.Avalonia/Controls/Inference/OutpaintCard.axaml.cs
@@ -5,6 +5,6 @@ using StabilityMatrix.Core.Attributes;
 namespace StabilityMatrix.Avalonia.Controls.Inference;
 
 [RegisterTransient<OutpaintCard>]
-public partial class OutpaintCard : TemplatedControlBase  // MORA biti 'partial'
+public partial class OutpaintCard : TemplatedControlBase
 {
 }
