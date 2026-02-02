d44ffac642897b6f5fa57c66639c801229c21670
Domica <Domica@users.noreply.github.com>
2026-02-02 09:15:49 +0100

    Update InferenceImageOutpaintViewModel.cs

--- StabilityMatrix.Avalonia/ViewModels/Inference/InferenceImageOutpaintViewModel.cs
+++ StabilityMatrix.Avalonia/ViewModels/Inference/InferenceImageOutpaintViewModel.cs
@@ -1,19 +1,15 @@
 using System.Collections.Generic;
-using System.Linq;
-using StabilityMatrix.Avalonia.Models;  // DODAJ
-using StabilityMatrix.Avalonia.Models.Inference;
+using StabilityMatrix.Avalonia.Models;
 using StabilityMatrix.Avalonia.Services;
 using StabilityMatrix.Avalonia.ViewModels.Base;
 using StabilityMatrix.Avalonia.ViewModels.Inference.Modules;
 using StabilityMatrix.Core.Attributes;
-using StabilityMatrix.Core.Models;
 using StabilityMatrix.Core.Models.Api.Comfy;
-using StabilityMatrix.Core.Models.Api.Comfy.Nodes;
+using StabilityMatrix.Core.Services;
 
 namespace StabilityMatrix.Avalonia.ViewModels.Inference;
 
-// ISPRAVI View atribut - treba Views.Inference, ne samo Views
-[View(typeof(Views.Inference.InferenceImageOutpaintView))]  // ✅ ISPRAVI
+[View(typeof(Views.Inference.InferenceImageOutpaintView))]
 [ManagedService]
 [Transient]
 public partial class InferenceImageOutpaintViewModel : InferenceGenerationViewModelBase
@@ -22,44 +18,48 @@ public partial class InferenceImageOutpaintViewModel : InferenceGenerationViewMo
 
     public StackCardViewModel StackCardViewModel { get; }
 
-    /// <inheritdoc />
     public InferenceImageOutpaintViewModel(
-        ServiceManager<ViewModelBase> vmFactory,
-        IInferenceClientManager clientManager
+        IServiceManager<ViewModelBase> vmFactory,
+        IInferenceClientManager clientManager,
+        INotificationService notificationService,
+        ISettingsManager settingsManager,
+        RunningPackageService runningPackageService
     )
-        : base(vmFactory, clientManager)
+        : base(vmFactory, clientManager, notificationService, settingsManager, runningPackageService)
     {
         StackCardViewModel = vmFactory.Get<StackCardViewModel>();
 
-        // Add by default the original cards as steps
-        var samplerModule = vmFactory.Get<SamplerModule>(module =>
+        // Add modules as cards
+        var samplerCard = vmFactory.Get<SamplerCardViewModel>(sampler =>
         {
-            module.IsDenoiseEnabled = true;
+            sampler.IsDenoiseStrengthEnabled = true;
         });
 
-        ModulesCardViewModel.AddModule<HiresFixModule>(module =>
-        {
-            module.IsEnabled = false;
-        });
-
-        // Add outpaint module
-        ModulesCardViewModel.AddModule<OutpaintModule>(module =>
-        {
-            module.IsEnabled = true; // Enabled by default
-        });
-
-        ModulesCardViewModel.AddModule(samplerModule);
+        StackCardViewModel.AddCards(
+            vmFactory.Get<SelectImageCardViewModel>(),
+            vmFactory.Get<OutpaintCardViewModel>(),
+            vmFactory.Get<PromptCardViewModel>(),
+            samplerCard,
+            vmFactory.Get<ModelCardViewModel>(),
+            vmFactory.Get<SeedCardViewModel>()
+        );
     }
 
-    /// <inheritdoc />
     protected override void BuildPrompt(BuildPromptEventArgs args)
     {
         var builder = args.Builder;
         var nodes = builder.Nodes;
 
-        // Get selected image
-        var imageSource = builder.GetPrimaryInputImage();
-        if (imageSource is null)
+        // Get cards
+        var selectImageCard = StackCardViewModel.GetCard<SelectImageCardViewModel>();
+        var outpaintCard = StackCardViewModel.GetCard<OutpaintCardViewModel>();
+        var promptCard = StackCardViewModel.GetCard<PromptCardViewModel>();
+        var samplerCard = StackCardViewModel.GetCard<SamplerCardViewModel>();
+        var modelCard = StackCardViewModel.GetCard<ModelCardViewModel>();
+        var seedCard = StackCardViewModel.GetCard<SeedCardViewModel>();
+
+        // Validate input image
+        if (selectImageCard?.ImageSource?.LocalFile is not { } imageFile)
         {
             return;
         }
@@ -69,92 +69,106 @@ public partial class InferenceImageOutpaintViewModel : InferenceGenerationViewMo
             new ComfyNodeBuilder.LoadImage
             {
                 Name = "LoadImage",
-                Image = imageSource.LocalFile?.ToString() ?? imageSource.RemoteUrl ?? ""
+                Image = imageFile.Name
             }
         );
 
-        // Get outpaint settings
-        var outpaintModule = ModulesCardViewModel.GetModule<OutpaintModule>();
-        if (outpaintModule?.IsEnabled == true)
-        {
-            // Pad Image for Outpainting
-            var padImage = nodes.AddNamedNode(
-                ComfyNodeBuilder
-                    .NamedNode("PadImageForOutpainting")
-                    .WithInput("image", loadImage.Output1!)
-                    .WithInput("left", outpaintModule.ExpandLeft)
-                    .WithInput("right", outpaintModule.ExpandRight)
-                    .WithInput("top", outpaintModule.ExpandTop)
-                    .WithInput("bottom", outpaintModule.ExpandBottom)
-                    .WithInput("feathering", outpaintModule.Feathering)
-            );
+        // Outpaint (Pad Image) - using InpaintModelConditioning node as example
+        // Note: This assumes you have a custom ComfyUI node for outpainting
+        // You may need to install a custom node pack for outpainting functionality
+        var padImage = nodes.AddNamedNode(
+            ComfyNodeBuilder.NamedNode("ImagePadForOutpaint")
+        );
+        padImage.InputFrom("image", loadImage.Output1!);
+        padImage.Input("left", outpaintCard?.ExpandLeft ?? 0);
+        padImage.Input("right", outpaintCard?.ExpandRight ?? 0);
+        padImage.Input("top", outpaintCard?.ExpandTop ?? 0);
+        padImage.Input("bottom", outpaintCard?.ExpandBottom ?? 0);
+        padImage.Input("feathering", outpaintCard?.Feathering ?? 40);
 
-            // Update primary image to padded version
-            builder.Connections.Primary.Image = padImage.Output1;
-            builder.Connections.PrimarySize = builder
-                .Connections
-                .PrimarySize
-                .WithWidth(
-                    builder.Connections.PrimarySize.Width
-                        + outpaintModule.ExpandLeft
-                        + outpaintModule.ExpandRight
-                )
-                .WithHeight(
-                    builder.Connections.PrimarySize.Height
-                        + outpaintModule.ExpandTop
-                        + outpaintModule.ExpandBottom
-                );
-        }
-        else
-        {
-            builder.Connections.Primary = builder.Connections.Primary with
+        // Load Checkpoint
+        var checkpoint = nodes.AddTypedNode(
+            new ComfyNodeBuilder.CheckpointLoaderSimple
             {
-                Image = loadImage.Output1
-            };
-        }
-
-        // Apply modules
-        var moduleApplySteps = ModulesCardViewModel.GetModuleApplySteps();
-        builder.Connections = moduleApplySteps.Apply(
-            builder,
-            builder.Connections,
-            ModuleApplyStepTemporaryArgs.Default
+                Name = "CheckpointLoader",
+                CkptName = modelCard?.SelectedModel?.RelativePath ?? ""
+            }
         );
 
-        // Get output image node
-        var outputImage = builder.Connections.Primary.Image;
-        if (outputImage is null)
-        {
-            return;
-        }
+        // CLIP Text Encode - Positive
+        var positivePrompt = nodes.AddTypedNode(
+            new ComfyNodeBuilder.CLIPTextEncode
+            {
+                Name = "PositivePrompt",
+                Clip = checkpoint.Output2!,
+                Text = promptCard?.PositivePrompt ?? ""
+            }
+        );
+
+        // CLIP Text Encode - Negative
+        var negativePrompt = nodes.AddTypedNode(
+            new ComfyNodeBuilder.CLIPTextEncode
+            {
+                Name = "NegativePrompt",
+                Clip = checkpoint.Output2!,
+                Text = promptCard?.NegativePrompt ?? ""
+            }
+        );
+
+        // VAE Encode
+        var vaeEncode = nodes.AddTypedNode(
+            new ComfyNodeBuilder.VAEEncode
+            {
+                Name = "VAEEncode",
+                Pixels = padImage.Output["IMAGE"]!,
+                Vae = checkpoint.Output3!
+            }
+        );
+
+        // KSampler
+        var sampler = nodes.AddTypedNode(
+            new ComfyNodeBuilder.KSampler
+            {
+                Name = "KSampler",
+                Model = checkpoint.Output1!,
+                Seed = seedCard?.Seed ?? 0,
+                Steps = samplerCard?.Steps ?? 20,
+                Cfg = samplerCard?.CfgScale ?? 7.0,
+                SamplerName = samplerCard?.SelectedSampler?.Name ?? "euler",
+                Scheduler = samplerCard?.SelectedScheduler?.Name ?? "normal",
+                Positive = positivePrompt.Output!,
+                Negative = negativePrompt.Output!,
+                LatentImage = vaeEncode.Output!,
+                Denoise = samplerCard?.DenoiseStrength ?? 1.0
+            }
+        );
+
+        // VAE Decode
+        var vaeDecode = nodes.AddTypedNode(
+            new ComfyNodeBuilder.VAEDecode
+            {
+                Name = "VAEDecode",
+                Samples = sampler.Output!,
+                Vae = checkpoint.Output3!
+            }
+        );
 
         // Preview Image
         nodes.AddTypedNode(
             new ComfyNodeBuilder.PreviewImage
             {
                 Name = "PreviewImage",
-                Images = outputImage
-            }
-        );
-
-        // Save Image
-        nodes.AddTypedNode(
-            new ComfyNodeBuilder.SaveImage
-            {
-                Name = "SaveImage",
-                Images = outputImage,
-                FilenamePrefix = "outpaint"
+                Images = vaeDecode.Output!
             }
         );
     }
 
-    /// <inheritdoc />
     protected override IEnumerable<ImageSource> GetInputImages()
     {
         var selectImageCard = StackCardViewModel.GetCard<SelectImageCardViewModel>();
-    if (selectImageCard?.ImageSource?.LocalFile is { } localFile)
-    {
-        yield return new ImageSource(localFile);
-    }
+        if (selectImageCard?.ImageSource is { } imageSource)
+        {
+            yield return imageSource;
+        }
     }
 }
