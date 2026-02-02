84e14f9a675d2e4cac1b867a5c8d1993d3ab0d42
Domica <Domica@users.noreply.github.com>
2026-02-02 08:29:30 +0100

    Create OutpaintCard.axaml.cs
    Add Outpaint Inference screen

---
+++ StabilityMatrix.Avalonia/Controls/Inference/OutpaintCard.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls.Primitives;
+using Injectio.Attributes;
+using StabilityMatrix.Core.Attributes;
+
+namespace StabilityMatrix.Avalonia.Controls.Inference;
+
+[RegisterTransient<OutpaintCard>]
+public class OutpaintCard : TemplatedControlBase
+{
+}
