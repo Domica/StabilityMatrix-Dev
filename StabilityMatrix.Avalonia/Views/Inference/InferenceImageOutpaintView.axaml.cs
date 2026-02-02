a0c89d68a4ff50c323534c953a6f04e922d010bd
Domica <Domica@users.noreply.github.com>
2026-02-02 09:16:52 +0100

    Update InferenceImageOutpaintView.axaml.cs

--- StabilityMatrix.Avalonia/Views/Inference/InferenceImageOutpaintView.axaml.cs
+++ StabilityMatrix.Avalonia/Views/Inference/InferenceImageOutpaintView.axaml.cs
@@ -1,10 +1,10 @@
-using Avalonia.Controls;
+using StabilityMatrix.Avalonia.Controls;
 using StabilityMatrix.Core.Attributes;
 
 namespace StabilityMatrix.Avalonia.Views.Inference;
 
 [View(typeof(ViewModels.Inference.InferenceImageOutpaintViewModel))]
-public partial class InferenceImageOutpaintView : UserControl
+public partial class InferenceImageOutpaintView : UserControlBase  // ✅ UserControlBase
 {
     public InferenceImageOutpaintView()
     {
