7099089f14c462762306bcd8c54d90f1849b3eb2
Domica <Domica@users.noreply.github.com>
2026-02-02 08:33:29 +0100

    Update ComfyUI.cs
    Add Outpaint Inference screen

--- StabilityMatrix.Core/Models/Packages/ComfyUI.cs
+++ StabilityMatrix.Core/Models/Packages/ComfyUI.cs
@@ -129,6 +129,11 @@ public class ComfyUI(
                     TargetRelativePaths = ["models/upscale_models"],
                     ConfigDocumentPaths = ["upscale_models"],
                 },
+                new SharedFolderLayoutRule
+                {
+                        SourceTypes = [SharedFolderType.Outpainting],
+                        TargetRelativePaths = ["models/outpainting"]
+                },
                 new SharedFolderLayoutRule // Hypernetworks
                 {
                     SourceTypes = [SharedFolderType.Hypernetwork],
