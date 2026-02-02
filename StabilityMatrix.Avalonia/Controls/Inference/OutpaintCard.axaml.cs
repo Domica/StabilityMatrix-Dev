0b351144b29d4e086c39b699dc4be6e319962fd4
Domica <Domica@users.noreply.github.com>
2026-02-02 19:44:27 +0100

    Update OutpaintCard.axaml.cs

--- StabilityMatrix.Avalonia/Controls/Inference/OutpaintCard.axaml.cs
+++ StabilityMatrix.Avalonia/Controls/Inference/OutpaintCard.axaml.cs
@@ -1,10 +1,17 @@
+using System;
 using Avalonia.Controls.Primitives;
 using Injectio.Attributes;
 using StabilityMatrix.Core.Attributes;
 
-namespace StabilityMatrix.Avalonia.Controls.Inference;
-
-[RegisterTransient<OutpaintCard>]
-public partial class OutpaintCard : UserControlBase
+namespace StabilityMatrix.Avalonia.Controls.Inference
 {
+    [RegisterTransient<OutpaintCard>]
+    public partial class OutpaintCard : UserControlBase
+    {
+        public OutpaintCard()
+        {
+            Console.WriteLine("OutpaintCard VIEW LOADED");
+            InitializeComponent();
+        }
+    }
 }
