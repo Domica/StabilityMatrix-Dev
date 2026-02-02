c652e38c5b01c47b4aae54d613b21140d0389991
Domica <Domica@users.noreply.github.com>
2026-02-02 09:30:29 +0100

    Update InferenceImageOutpaintViewModel.cs

--- StabilityMatrix.Avalonia/ViewModels/Inference/InferenceImageOutpaintViewModel.cs
+++ StabilityMatrix.Avalonia/ViewModels/Inference/InferenceImageOutpaintViewModel.cs
@@ -4,7 +4,8 @@ using StabilityMatrix.Avalonia.Services;
 using StabilityMatrix.Avalonia.ViewModels.Base;
 using StabilityMatrix.Avalonia.ViewModels.Inference.Modules;
 using StabilityMatrix.Core.Attributes;
-using StabilityMatrix.Core.Models.Api.Comfy;
+using StabilityMatrix.Core.Models.Api.Comfy.Nodes;
+using StabilityMatrix.Core.Models.Api.Comfy.NodeTypes;
 using StabilityMatrix.Core.Services;
 
 namespace StabilityMatrix.Avalonia.ViewModels.Inference;
@@ -29,7 +30,6 @@ public partial class InferenceImageOutpaintViewModel : InferenceGenerationViewMo
     {
         StackCardViewModel = vmFactory.Get<StackCardViewModel>();
 
-        // Add modules as cards
         var samplerCard = vmFactory.Get<SamplerCardViewModel>(sampler =>
         {
             sampler.IsDenoiseStrengthEnabled = true;
@@ -47,10 +47,8 @@ public partial class InferenceImageOutpaintViewModel : InferenceGenerationViewMo
 
     protected override void BuildPrompt(BuildPromptEventArgs args)
     {
-        var builder = args.Builder;
-        var nodes = builder.Nodes;
+        var nodes = args.Builder.Nodes;
 
-        // Get cards
         var selectImageCard = StackCardViewModel.GetCard<SelectImageCardViewModel>();
         var outpaintCard = StackCardViewModel.GetCard<OutpaintCardViewModel>();
         var promptCard = StackCardViewModel.GetCard<PromptCardViewModel>();
@@ -58,13 +56,9 @@ public partial class InferenceImageOutpaintViewModel : InferenceGenerationViewMo
         var modelCard = StackCardViewModel.GetCard<ModelCardViewModel>();
         var seedCard = StackCardViewModel.GetCard<SeedCardViewModel>();
 
-        // Validate input image
         if (selectImageCard?.ImageSource?.LocalFile is not { } imageFile)
-        {
             return;
-        }
 
-        // Load Image
         var loadImage = nodes.AddTypedNode(
             new ComfyNodeBuilder.LoadImage
             {
@@ -73,20 +67,22 @@ public partial class InferenceImageOutpaintViewModel : InferenceGenerationViewMo
             }
         );
 
-        // Outpaint (Pad Image) - using InpaintModelConditioning node as example
-        // Note: This assumes you have a custom ComfyUI node for outpainting
-        // You may need to install a custom node pack for outpainting functionality
         var padImage = nodes.AddNamedNode(
-            ComfyNodeBuilder.NamedNode("ImagePadForOutpaint")
+            new NamedComfyNode<ImageNodeConnection>("PadImage")
+            {
+                ClassType = "ImagePadForOutpaint",
+                Inputs = new Dictionary<string, object?>
+                {
+                    ["image"] = loadImage.Output.Data,
+                    ["left"] = outpaintCard?.ExpandLeft ?? 0,
+                    ["right"] = outpaintCard?.ExpandRight ?? 0,
+                    ["top"] = outpaintCard?.ExpandTop ?? 0,
+                    ["bottom"] = outpaintCard?.ExpandBottom ?? 0,
+                    ["feathering"] = outpaintCard?.Feathering ?? 40
+                }
+            }
         );
-        padImage.InputFrom("image", loadImage.Output1!);
-        padImage.Input("left", outpaintCard?.ExpandLeft ?? 0);
-        padImage.Input("right", outpaintCard?.ExpandRight ?? 0);
-        padImage.Input("top", outpaintCard?.ExpandTop ?? 0);
-        padImage.Input("bottom", outpaintCard?.ExpandBottom ?? 0);
-        padImage.Input("feathering", outpaintCard?.Feathering ?? 40);
 
-        // Load Checkpoint
         var checkpoint = nodes.AddTypedNode(
             new ComfyNodeBuilder.CheckpointLoaderSimple
             {
@@ -95,70 +91,64 @@ public partial class InferenceImageOutpaintViewModel : InferenceGenerationViewMo
             }
         );
 
-        // CLIP Text Encode - Positive
         var positivePrompt = nodes.AddTypedNode(
             new ComfyNodeBuilder.CLIPTextEncode
             {
                 Name = "PositivePrompt",
-                Clip = checkpoint.Output2!,
-                Text = promptCard?.PositivePrompt ?? ""
+                Clip = checkpoint.Output2,
+                Text = promptCard?.PromptDocument.Text ?? ""
             }
         );
 
-        // CLIP Text Encode - Negative
         var negativePrompt = nodes.AddTypedNode(
             new ComfyNodeBuilder.CLIPTextEncode
             {
                 Name = "NegativePrompt",
-                Clip = checkpoint.Output2!,
-                Text = promptCard?.NegativePrompt ?? ""
+                Clip = checkpoint.Output2,
+                Text = promptCard?.NegativePromptDocument.Text ?? ""
             }
         );
 
-        // VAE Encode
         var vaeEncode = nodes.AddTypedNode(
             new ComfyNodeBuilder.VAEEncode
             {
                 Name = "VAEEncode",
-                Pixels = padImage.Output["IMAGE"]!,
-                Vae = checkpoint.Output3!
+                Pixels = padImage.Output,
+                Vae = checkpoint.Output3
             }
         );
 
-        // KSampler
         var sampler = nodes.AddTypedNode(
             new ComfyNodeBuilder.KSampler
             {
                 Name = "KSampler",
-                Model = checkpoint.Output1!,
+                Model = checkpoint.Output1,
                 Seed = seedCard?.Seed ?? 0,
                 Steps = samplerCard?.Steps ?? 20,
                 Cfg = samplerCard?.CfgScale ?? 7.0,
                 SamplerName = samplerCard?.SelectedSampler?.Name ?? "euler",
                 Scheduler = samplerCard?.SelectedScheduler?.Name ?? "normal",
-                Positive = positivePrompt.Output!,
-                Negative = negativePrompt.Output!,
-                LatentImage = vaeEncode.Output!,
+                Positive = positivePrompt.Output,
+                Negative = negativePrompt.Output,
+                LatentImage = vaeEncode.Output,
                 Denoise = samplerCard?.DenoiseStrength ?? 1.0
             }
         );
 
-        // VAE Decode
         var vaeDecode = nodes.AddTypedNode(
             new ComfyNodeBuilder.VAEDecode
             {
                 Name = "VAEDecode",
-                Samples = sampler.Output!,
-                Vae = checkpoint.Output3!
+                Samples = sampler.Output,
+                Vae = checkpoint.Output3
             }
         );
 
-        // Preview Image
         nodes.AddTypedNode(
             new ComfyNodeBuilder.PreviewImage
             {
                 Name = "PreviewImage",
-                Images = vaeDecode.Output!
+                Images = vaeDecode.Output
             }
         );
     }
@@ -167,8 +157,6 @@ public partial class InferenceImageOutpaintViewModel : InferenceGenerationViewMo
     {
         var selectImageCard = StackCardViewModel.GetCard<SelectImageCardViewModel>();
         if (selectImageCard?.ImageSource is { } imageSource)
-        {
             yield return imageSource;
-        }
     }
 }
