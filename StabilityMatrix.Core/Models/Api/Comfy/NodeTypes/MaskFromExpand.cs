4c42826381c6457307ae4f7968c07dd7a421b745
Domica <Domica@users.noreply.github.com>
2026-02-03 12:55:00 +0100

    Update MaskFromExpand.cs

--- StabilityMatrix.Core/Models/Api/Comfy/NodeTypes/MaskFromExpand.cs
+++ StabilityMatrix.Core/Models/Api/Comfy/NodeTypes/MaskFromExpand.cs
@@ -1,34 +1,22 @@
 using StabilityMatrix.Core.Models.Api.Comfy;
 using StabilityMatrix.Core.Models.Api.Comfy.Nodes;
-
+using StabilityMatrix.Core.Models.Api.Comfy.NodeTypes.Connections;
 
 namespace StabilityMatrix.Core.Models.Api.Comfy.NodeTypes;
 
-public class MaskFromExpand : ComfyNode
+public record MaskFromExpand : ComfyNode
 {
     public MaskFromExpand()
     {
         ClassType = "MaskFromExpand";
     }
 
-    public ComfyNodeInput<IntNodeConnection> Width
-        => Input<IntNodeConnection>("width");
+    public ComfyNodeInput<IntNodeConnection> Width => Input<IntNodeConnection>("width");
+    public ComfyNodeInput<IntNodeConnection> Height => Input<IntNodeConnection>("height");
+    public ComfyNodeInput<IntNodeConnection> Left => Input<IntNodeConnection>("left");
+    public ComfyNodeInput<IntNodeConnection> Right => Input<IntNodeConnection>("right");
+    public ComfyNodeInput<IntNodeConnection> Top => Input<IntNodeConnection>("top");
+    public ComfyNodeInput<IntNodeConnection> Bottom => Input<IntNodeConnection>("bottom");
 
-    public ComfyNodeInput<IntNodeConnection> Height
-        => Input<IntNodeConnection>("height");
-
-    public ComfyNodeInput<IntNodeConnection> Left
-        => Input<IntNodeConnection>("left");
-
-    public ComfyNodeInput<IntNodeConnection> Right
-        => Input<IntNodeConnection>("right");
-
-    public ComfyNodeInput<IntNodeConnection> Top
-        => Input<IntNodeConnection>("top");
-
-    public ComfyNodeInput<IntNodeConnection> Bottom
-        => Input<IntNodeConnection>("bottom");
-
-    public ComfyNodeOutput<MaskNodeConnection> Mask
-        => Output<MaskNodeConnection>(0);
+    public ComfyNodeOutput<MaskNodeConnection> Mask => Output<MaskNodeConnection>(0);
 }
