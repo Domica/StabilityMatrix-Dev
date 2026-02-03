6583755645186d3e0e924c913cec532c52647c6b
Domica <Domica@users.noreply.github.com>
2026-02-03 12:35:02 +0100

    Create LatentComposite.cs

---
+++ StabilityMatrix.Core/Models/Api/Comfy/NodeTypes/LatentComposite.cs
@@ -0,0 +1,23 @@
+using StabilityMatrix.Core.Models.Api.Comfy;
+
+namespace StabilityMatrix.Core.Models.Api.Comfy.NodeTypes;
+
+public class LatentComposite : ComfyNode
+{
+    public LatentComposite()
+    {
+        ClassType = "LatentComposite";
+    }
+
+    public ComfyNodeInput<LatentNodeConnection> Original
+        => Input<LatentNodeConnection>("original");
+
+    public ComfyNodeInput<LatentNodeConnection> Generated
+        => Input<LatentNodeConnection>("generated");
+
+    public ComfyNodeInput<MaskNodeConnection> Mask
+        => Input<MaskNodeConnection>("mask");
+
+    public ComfyNodeOutput<LatentNodeConnection> Output
+        => Output<LatentNodeConnection>(0);
+}
