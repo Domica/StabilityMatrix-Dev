178d31696cb5b10a0a48e481a1745bc0ed88ec34
Domica <Domica@users.noreply.github.com>
2026-02-03 12:55:09 +0100

    Update LatentComposite.cs

--- StabilityMatrix.Core/Models/Api/Comfy/NodeTypes/LatentComposite.cs
+++ StabilityMatrix.Core/Models/Api/Comfy/NodeTypes/LatentComposite.cs
@@ -1,25 +1,19 @@
 using StabilityMatrix.Core.Models.Api.Comfy;
 using StabilityMatrix.Core.Models.Api.Comfy.Nodes;
-
+using StabilityMatrix.Core.Models.Api.Comfy.NodeTypes.Connections;
 
 namespace StabilityMatrix.Core.Models.Api.Comfy.NodeTypes;
 
-public class LatentComposite : ComfyNode
+public record LatentComposite : ComfyNode
 {
     public LatentComposite()
     {
         ClassType = "LatentComposite";
     }
 
-    public ComfyNodeInput<LatentNodeConnection> Original
-        => Input<LatentNodeConnection>("original");
+    public ComfyNodeInput<LatentNodeConnection> Original => Input<LatentNodeConnection>("original");
+    public ComfyNodeInput<LatentNodeConnection> Generated => Input<LatentNodeConnection>("generated");
+    public ComfyNodeInput<MaskNodeConnection> Mask => Input<MaskNodeConnection>("mask");
 
-    public ComfyNodeInput<LatentNodeConnection> Generated
-        => Input<LatentNodeConnection>("generated");
-
-    public ComfyNodeInput<MaskNodeConnection> Mask
-        => Input<MaskNodeConnection>("mask");
-
-    public ComfyNodeOutput<LatentNodeConnection> Output
-        => Output<LatentNodeConnection>(0);
+    public ComfyNodeOutput<LatentNodeConnection> Output => Output<LatentNodeConnection>(0);
 }
