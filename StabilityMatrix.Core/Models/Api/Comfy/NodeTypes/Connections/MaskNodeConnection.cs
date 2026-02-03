3d9d61883313ae1130f56044aedef7b668e1f965
Domica <Domica@users.noreply.github.com>
2026-02-03 12:36:50 +0100

    Create MaskNodeConnection.cs

---
+++ StabilityMatrix.Core/Models/Api/Comfy/NodeTypes/Connections/MaskNodeConnection.cs
@@ -0,0 +1,8 @@
+using StabilityMatrix.Core.Models.Api.Comfy;
+
+namespace StabilityMatrix.Core.Models.Api.Comfy.NodeTypes;
+
+public class MaskNodeConnection : ComfyNodeConnection
+{
+    public override string Type => "MASK";
+}
