74382b68b07599b68519ad53be5857891f61cf79
Domica <Domica@users.noreply.github.com>
2026-02-03 12:51:01 +0100

    Update MaskNodeConnection.cs

--- StabilityMatrix.Core/Models/Api/Comfy/NodeTypes/Connections/MaskNodeConnection.cs
+++ StabilityMatrix.Core/Models/Api/Comfy/NodeTypes/Connections/MaskNodeConnection.cs
@@ -1,6 +1,7 @@
 using StabilityMatrix.Core.Models.Api.Comfy;
+using StabilityMatrix.Core.Models.Api.Comfy.Nodes;
 
-namespace StabilityMatrix.Core.Models.Api.Comfy.NodeTypes;
+namespace StabilityMatrix.Core.Models.Api.Comfy.NodeTypes.Connections;
 
 public class MaskNodeConnection : ComfyNodeConnection
 {
