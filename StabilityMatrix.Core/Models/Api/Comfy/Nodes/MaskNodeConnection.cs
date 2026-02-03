928d9fa23aba22acce1f8a1c4d9d35c1143113da
Domica <Domica@users.noreply.github.com>
2026-02-03 14:16:08 +0100

    Delete StabilityMatrix.Core/Models/Api/Comfy/Nodes/MaskNodeConnection.cs

--- StabilityMatrix.Core/Models/Api/Comfy/Nodes/MaskNodeConnection.cs
+++
@@ -1,8 +0,0 @@
-using StabilityMatrix.Core.Models.Api.Comfy;
-
-namespace StabilityMatrix.Core.Models.Api.Comfy.Nodes;
-
-public record MaskNodeConnection : ComfyNodeConnection
-{
-    public override string Type => "MASK";
-}
